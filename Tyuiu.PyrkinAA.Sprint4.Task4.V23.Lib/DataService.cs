1604754db3cdf0f9ec7ae2b59cd1f5dae70bace1
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-07 18:57:35 +0500

    1

---
+++ Tyuiu.PyrkinAA.Sprint4.Task4.V23.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.PyrkinAA.Sprint4.Task4.V23.Lib
+{
+    public class DataService : ISprint4Task4V23
+    {
+        public int Calculate(int[,] array)
+        {
+            int sumOdd = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] % 2 != 0)
+                    {
+                        sumOdd += array[i, j];
+                    }
+                }
+            }
+
+            return sumOdd;
+        }
+    }
+}
---
+++ Tyuiu.PyrkinAA.Sprint4.Task4.V23/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.PyrkinAA.Sprint4.Task4.V23.Lib;
+namespace Tyuiu.PyrkinAA.Sprint4.Task4.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[,]
+            {
+                { 8, 7, 5, 8, 7 },
+                { 8, 3, 3, 6, 4 },
+                { 4, 4, 5, 5, 4 },
+                { 3, 4, 7, 7, 3 },
+                { 6, 8, 3, 6, 7 }
+            };
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив 5x5:");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int res = ds.Calculate(array);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Сумма нечётных элементов массива = {res}");
+            Console.ReadKey();
+        }
+    }
+}
